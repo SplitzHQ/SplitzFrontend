5681168c64b2ee80d5b42563e2002410ba2000a4
arition <aritionkb@gmail.com>
2025-05-28 20:20:16 -0700

    fix: do not show empty icon placeholder

--- src/components/SButton/SButtonBase.vue
+++ src/components/SButton/SButtonBase.vue
@@ -23,11 +23,11 @@ const { color, variant, size, disabled, loading, iconOnly } = defineProps<Button
     ]"
   >
     <PhCircleNotch v-if="loading" class="icon shrink-0 animate-spin" />
-    <div v-if="$slots['icon-left']" class="icon shrink-0 relative">
+    <div v-if="$slots['icon-left']" class="icon shrink-0 relative empty:hidden">
       <slot name="icon-left" />
     </div>
     <slot />
-    <div v-if="$slots['icon-right']" class="icon shrink-0 relative">
+    <div v-if="$slots['icon-right']" class="icon shrink-0 relative empty:hidden">
       <slot name="icon-right" />
     </div>
   </div>
